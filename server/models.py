4d5aa768c0b6a9ae7550a38fc66f50617f510e3f
+ <+>
2024-07-16 12:04:35 +0300

    feat:Add sqlite db

--- server/models.py
+++ server/models.py
@@ -8,7 +8,6 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
-    phone = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     projects = db.relationship('Project', cascade='all, delete-orphan', back_populates='user')
     projects = association_proxy('bookings', 'project')
