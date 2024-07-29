d2ffc816564386551e5e76a693304c2445a0b27f
+ <+>
2024-07-29 12:11:07 +0300

    refactor:modify signup component

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import * as Yup from 'yup';
 import {useFormik} from 'formik';
 import axios from 'axios';
-import sha1 from 'sha1';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
