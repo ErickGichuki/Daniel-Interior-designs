26c327cb1a214fe73490733a9dd4f32e98f645dd
+ <+>
2024-09-04 17:56:25 +0300

    refactor:change route for the passwords reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Services from './pages/Services';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import ResetPassword from './components/ForgotPassword';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import ResetPassword from "./components/ForgotPassword";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Signup/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/services' element={<Services/>} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/reset_password' element={<ResetPassword/>}/>
+        <Route path="/" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
